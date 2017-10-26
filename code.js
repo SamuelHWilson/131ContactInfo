7baa31d7f354e6840b388b824142f2a0699bb2a1
Unknown <swilsondev@outlook.com>
2017-10-26 12:08:55 -0500

    Cleaned up popup layout, added header.

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
 function OpenContact() {
-    window.open("contact.html", "contact-window", "width=600,height=400,resizable=no,menubar=no,location=no,toolbar=no");
+    window.open("contact.html", "contact-window", "width=600,height=440,resizable=no,menubar=no,location=no,toolbar=no");
 }
