4f1b71becd30547ca29be77e4dfe1647b5a64f98
Unknown <swilsondev@outlook.com>
2017-10-26 13:14:16 -0500

    Added capability in index for displaying arbitrary contacts.

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
+function DisplaySelectedContact(name ,image, phone, email) {
+    $("name").text(name);
+    $("img").attr("src", image);
+    $("phone").text(phone);
+    $("email").text(email);
+    
+}
+
 function OpenContact() {
     window.open("contact.html", "contact-window", "width=700,height=520,scrollbars=yes");
 }
